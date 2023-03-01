fc4e0897295d838a42414bd4cd4afeaaab9635b7
Burle-vineeth <109106500+Burle-vineeth@users.noreply.github.com>
2023-03-01 14:28:42 +0530

    Add files via upload

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,12 @@
 let player1 = [], player2 = [];
 let counter = 0;
 let vineeth = true;
+let gameStart = false;
 let block = document.querySelectorAll('.blocks');
 for (let i = 0; i < block.length; i++) {
     // console.log(block[i]);
     block[i].addEventListener('click', function () {
-        if (block[i].children.length == 0) {
+        if (block[i].children.length == 0 && gameStart) {
             let ele = document.createElement('span');
             if (counter % 2 == 0 && vineeth) {
                 ele.innerHTML = "x";
@@ -31,12 +32,14 @@ for (let i = 0; i < block.length; i++) {
 }
 
 function player() {
-    if (counter % 2 == 0) {
-        document.querySelector('.player1').innerHTML = "Player1 Turn";
-        document.querySelector('.player2').innerHTML = "";
-    } else {
-        document.querySelector('.player2').innerHTML = "Player2 Turn";
-        document.querySelector('.player1').innerHTML = "";
+    if (gameStart) {
+        if (counter % 2 == 0) {
+            document.querySelector('.player1').innerHTML = "Player1 Turn";
+            document.querySelector('.player2').innerHTML = "";
+        } else {
+            document.querySelector('.player2').innerHTML = "Player2 Turn";
+            document.querySelector('.player1').innerHTML = "";
+        }
     }
 }
 
@@ -55,6 +58,8 @@ function checkResult() {
         document.querySelector('.player1').innerHTML = "";
         counter = "vineeth";
         vineeth = false;
+        gameStart = false;
+        winningPlay();
     }
 
     if ((player2.includes(0) && player2.includes(1) && player2.includes(2)) ||
@@ -71,6 +76,8 @@ function checkResult() {
         document.querySelector('.player1').innerHTML = "";
         counter = "vineeth";
         vineeth = false;
+        gameStart = false;
+        winningPlay();
     }
 }
 
@@ -78,4 +85,45 @@ function tie() {
     document.getElementById('result').innerHTML = "Draw";
     document.querySelector('.player2').innerHTML = "";
     document.getElementById('result').style.color = "white";
+}
+
+function start() {
+    gameStart = true;
+    player();
+    document.getElementById('start').style.color = 'blue';
+    setTimeout(function () {
+        document.getElementById('start').style.color = '';
+    }, 300);
+    document.getElementById('clickMe').innerHTML = "";
+}
+
+function reset() {
+    gameStart = false;
+    document.getElementById('reset').style.color = 'pink';
+    setTimeout(function () {
+        document.getElementById('reset').style.color = '';
+    }, 300);
+    player1 = [];
+    player2 = [];
+    for (let i = 0; i < block.length; i++) {
+        block[i].innerHTML = '';
+    }
+    document.querySelector('.player2').innerHTML = "";
+    document.querySelector('.player1').innerHTML = "";
+    counter = 0;
+    vineeth = true;
+    document.getElementById('clickMe').innerHTML = "For to start the game click the start button";
+    document.getElementById('result').innerHTML = "";
+}
+
+function play() {
+    if (gameStart) {
+        var audio = document.getElementById("audio");
+        audio.play();
+    }
+}
+
+function winningPlay() {
+    var audio1 = document.getElementById('audio1');
+    audio1.play();
 }
